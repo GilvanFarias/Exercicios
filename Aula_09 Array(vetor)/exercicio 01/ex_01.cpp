fafddafc6db3e1ac172657fc14bc86dd2e5c7409
Gilvan Farias <63085957+GilvanFarias@users.noreply.github.com>
2020-08-05 14:06:38 -0300

    05/08/2020

--- Aula_09 Array(vetor)/exercicio 01/ex_01.cpp
+++ Aula_09 Array(vetor)/exercicio 01/ex_01.cpp
@@ -7,12 +7,34 @@ int main(){
 	
 	setlocale(LC_ALL, "ptb");
 	
+	int numElementos = 5, i=0;
 	int vetor[10];
+	int num[10];
+	int tentativas=3;
 	
-	for(int i=0; i<10; i++){
+	for(int i=0; i<numElementos; i++){
 		
-		cout<< "Digite um número";
+		cout<< "Digite "<<i+1<< " número ";
 		cin>> vetor[i];
 		
+		system("CLS");
+		
+		
 	}
+			 
+		for(i; i<numElementos; i++){
+			
+			if(num[i] < 3){
+				cout<<"Digite umm número";
+				cin>>num[i];
+				cout<<" Você errou";
+			}else if(vetor[i] == num){
+			
+			cout<< "Parabéns você acertou\n";
+			cout<< " Esse número está na posição "<<num[i];
+			return 0;
+			
+			}
+		
+ 		}
 }
